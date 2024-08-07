f5c2a4d58974bfe63497c84f42b20f47a5377ddb
IoannaAbadjieva <160428557+IoannaAbadjieva@users.noreply.github.com>
2024-08-07 17:07:12 +0300

    terraform variables files added

--- main.tf
+++ main.tf
@@ -18,20 +18,20 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "arg" {
-  name     = "TaskBoardResourceGroup${random_integer.ri.result}"
-  location = "West Europe"
+  name     = "${var.resource_group_name}${random_integer.ri.result}"
+  location = var.resource_group_location
 }
 
 resource "azurerm_service_plan" "asp" {
-  name                = "TaskBoardServicePlan${random_integer.ri.result}"
+  name                = "${var.app_service_plan_name}${random_integer.ri.result}"
   resource_group_name = azurerm_resource_group.arg.name
   location            = azurerm_resource_group.arg.location
-  os_type             = "Linux"
-  sku_name            = "F1"
+  os_type             = var.app_service_os
+  sku_name            = var.app_service_plan_sku
 }
 
 resource "azurerm_linux_web_app" "alwa" {
-  name                = "TaskBoard${random_integer.ri.result}"
+  name                = "${var.app_service_name}${random_integer.ri.result}"
   resource_group_name = azurerm_resource_group.arg.name
   location            = azurerm_service_plan.asp.location
   service_plan_id     = azurerm_service_plan.asp.id
@@ -51,29 +51,29 @@ resource "azurerm_linux_web_app" "alwa" {
 
 resource "azurerm_app_service_source_control" "aassc" {
   app_id   = azurerm_linux_web_app.alwa.id
-  repo_url = "https://github.com/IoannaAbadjieva/TaskBoard.git"
-  branch   = "main"
+  repo_url = var.app_service_repo_url
+  branch   = var.app_service_branch
 }
 
 
 resource "azurerm_mssql_server" "ams" {
-  name                         = "sqlserver${random_integer.ri.result}"
+  name                         = "${var.sql_server_name}${random_integer.ri.result}"
   resource_group_name          = azurerm_resource_group.arg.name
   location                     = azurerm_resource_group.arg.location
-  version                      = "12.0"
-  administrator_login          = "sqladminun"
-  administrator_login_password = "thisIsKat11"
+  version                      = var.sql_server_version
+  administrator_login          = var.sql_server_admin_login
+  administrator_login_password = var.sql_server_admin_password
 }
 
 resource "azurerm_mssql_firewall_rule" "example" {
-  name             = "FirewallRule1"
+  name             = var.mssql_firewall_rule_name
   server_id        = azurerm_mssql_server.ams.id
   start_ip_address = "0.0.0.0"
   end_ip_address   = "0.0.0.0"
 }
 
 resource "azurerm_mssql_database" "amd" {
-  name           = "TaskBoardDB${random_integer.ri.result}"
+  name           = "${var.sql_database_name}${random_integer.ri.result}"
   server_id      = azurerm_mssql_server.ams.id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
   license_type   = "LicenseIncluded"
