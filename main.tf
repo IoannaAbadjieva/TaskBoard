a4827158eb2556d42556c6c5920555f99977d9fd
IoannaAbadjieva <160428557+IoannaAbadjieva@users.noreply.github.com>
2024-08-07 15:39:53 +0300

    main.tf added

---
+++ main.tf
@@ -0,0 +1,83 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.113.0"
+    }
+  }
+}
+
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "arg" {
+  name     = "TaskBoardResourceGroup${random_integer.ri.result}"
+  location = "West Europe"
+}
+
+resource "azurerm_service_plan" "asp" {
+  name                = "TaskBoardServicePlan${random_integer.ri.result}"
+  resource_group_name = azurerm_resource_group.arg.name
+  location            = azurerm_resource_group.arg.location
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+resource "azurerm_linux_web_app" "alwa" {
+  name                = "TaskBoard${random_integer.ri.result}"
+  resource_group_name = azurerm_resource_group.arg.name
+  location            = azurerm_service_plan.asp.location
+  service_plan_id     = azurerm_service_plan.asp.id
+
+  site_config {
+    application_stack {
+      dotnet_version = "6.0"
+    }
+    always_on = false
+  }
+  connection_string {
+    name  = "DefaultConnection"
+    type  = "SQLAzure"
+    value = "Data Source=tcp:${azurerm_mssql_server.ams.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.amd.name};User ID=${azurerm_mssql_server.ams.administrator_login};Password=${azurerm_mssql_server.ams.administrator_login_password};Trusted_Connection=False; MultipleActiveResultSets=True;"
+  }
+}
+
+resource "azurerm_app_service_source_control" "aassc" {
+  app_id   = azurerm_linux_web_app.alwa.id
+  repo_url = "https://github.com/IoannaAbadjieva/TaskBoard.git"
+  branch   = "main"
+}
+
+
+resource "azurerm_mssql_server" "ams" {
+  name                         = "sqlserver${random_integer.ri.result}"
+  resource_group_name          = azurerm_resource_group.arg.name
+  location                     = azurerm_resource_group.arg.location
+  version                      = "12.0"
+  administrator_login          = "sqladminun"
+  administrator_login_password = "thisIsKat11"
+}
+
+resource "azurerm_mssql_firewall_rule" "example" {
+  name             = "FirewallRule1"
+  server_id        = azurerm_mssql_server.ams.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
+
+resource "azurerm_mssql_database" "amd" {
+  name           = "TaskBoardDB${random_integer.ri.result}"
+  server_id      = azurerm_mssql_server.ams.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 2
+  sku_name       = "S0"
+  zone_redundant = false
+}
